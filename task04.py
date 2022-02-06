bb09e5337ae229bb75eb67f71d8e849f64e4d010
abi-mat <amatlaweng@gmail.com>
2022-02-06 23:17:28 +0200

    Function takes three numbers and returns maximum number

--- task04.py
+++
@@ -1,7 +0,0 @@
-def evenOrOdd(x):
-    if x%2 == 0:
-       print("even")
-    else:
-       print("odd")
-  
-#Call function evenOrOdd and input integer
---
+++ task06.py
@@ -0,0 +1,9 @@
+def maximum(x, y, z):
+    if x > y and x > z:
+        print(x)
+    elif y > x and y > z:
+        print(y)
+    elif z > y and z > x:
+        print(z)
+        
+#Call function maximum() 
