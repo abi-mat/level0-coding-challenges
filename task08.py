5d171ac1067c074819628cdf8e4c37a10238a34f
Suzan <96047840+abi-mat@users.noreply.github.com>
2022-02-07 20:36:36 +0200

    Create task08.py

--- task08.py
+++ task08.py
@@ -4,13 +4,15 @@ def time_convertor(t):
     elif t//60 ==1 and t%60 ==1:
         print("1 hour, 1 minute")
     elif t//60 ==1 and t%60 >1:
-        print("1 hour," + str(t%60) + "minutes")
+        print("1 hour," + str(t%60) + " minutes")
     elif t//60 >1 and t%60 ==0:
-        print(str(t//60) + "hours") 
+        print(str(t//60) + " hours") 
     elif t//60 >1 and t%60 ==1:
-        print(str(t//60) + "hours, 1 minute" )
+        print(str(t//60) + " hours, 1 minute" )
     elif t//60 >1 and t%60 >1:
-        print(str(t//60) + "hours," + str(t%60) + "minutes")
+        print(str(t//60) + " hours," + str(t%60) + " minutes")
+    elif t < 60:
+        print(str(t) + " minutes") 
         
-#Call function to convert time to hours and minutes 
+
 
