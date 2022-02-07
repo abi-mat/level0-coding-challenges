050319a5d42ed08668a499bd9e107e637209d0db
abi-mat <amatlaweng@gmail.com>
2022-02-07 03:33:01 +0200

    function takes in two strings and outputs common letters/characters

---
+++ task10.py
@@ -0,0 +1,7 @@
+def commonletters(x,y):
+    for i in x:
+        for j in y:
+            if j == i:
+                print("Common letters:" + i)
+
+#Call function commonletters and input two strings
