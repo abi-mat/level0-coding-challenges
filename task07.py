30ae64e443a00cc4d67dc346e8edd35e13b1c2c4
Suzan <96047840+abi-mat@users.noreply.github.com>
2022-02-08 10:04:14 +0200

    Update task07.py

--- task07.py
+++ task07.py
@@ -1,8 +1,9 @@
-#Convert celsius to fahrenheit
-def fahrenheit_temp(c):
-    print(c * 9/5 + 32)
-    
-# Convert fahrenheit to celsius
-def celsius_temp(f):
-    print((f - 32) * 5/9)
 
+def convert(celsius):
+    fahrenheit = celsius * 9/5 + 32
+    print(fahrenheit)
+
+
+def convert(fahrenheit):
+    celsius = (f - 32) * 5/9
+    print(celsius) 
