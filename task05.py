3cf96ae3aed7aed9c1cc0f07a7aa74c51d85a88f
Suzan <96047840+abi-mat@users.noreply.github.com>
2022-02-08 10:26:21 +0200

    Update task05.py

--- task05.py
+++ task05.py
@@ -1,4 +1,5 @@
 def triangle(x, y, z):
-    print(0.5 * (x+y+z)) 
-    
-#Call function triangle
+    semiperimeter = 0.5 * (x+y+z) 
+    s = semiperimeter
+    area = (s * ((s-x) * (s-y) * (s-z))) ** 0.5
+    print(area)
